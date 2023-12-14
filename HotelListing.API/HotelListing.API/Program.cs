e0aa4d3d7b53479485a731dbf3590ba706b29c23
snowman07 <domingo.arrbelrey@gmail.com>
2023-12-13 20:36:43 -0700

    Course1: added Serilog request logging

--- HotelListing.API/HotelListing.API/Program.cs
+++ HotelListing.API/HotelListing.API/Program.cs
@@ -31,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
+
 app.UseHttpsRedirection();
 
 // Telling the pipeline to use CORS policy that has a name "AllowAll"
