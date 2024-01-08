2b5f17b18ec3fb3a5b923dc0ce489213c58f02a9
snowman07 <domingo.arrbelrey@gmail.com>
2024-01-08 11:28:41 -0700

    Course1: 50 Setup User Identity Roles

--- HotelListing.API/HotelListing.API/Program.cs
+++ HotelListing.API/HotelListing.API/Program.cs
@@ -2,6 +2,7 @@ using HotelListing.API.Configurations;
 using HotelListing.API.Contracts;
 using HotelListing.API.Data;
 using HotelListing.API.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -14,6 +15,11 @@ builder.Services.AddDbContext<HotelListingDbContext>(options =>
 {
     options.UseSqlServer(connectionString);
 });
+
+//Setup User Identity Core
+builder.Services.AddIdentityCore<ApiUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<HotelListingDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
