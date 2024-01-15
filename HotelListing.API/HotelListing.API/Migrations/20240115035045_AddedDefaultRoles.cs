47f24951ec8ead568f38fdbce00f9175a1b6893e
snowman07 <domingo.arrbelrey@gmail.com>
2024-01-14 21:02:26 -0700

    Course1: 51. Add Default User Roles

---
+++ HotelListing.API/HotelListing.API/Migrations/20240115035045_AddedDefaultRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3ff2e50c-6905-4eeb-ac17-9ee7ab9c5022", null, "Administrator", "ADMINISTRATOR" },
+                    { "90c82976-5b02-4114-9a1b-c6f6999aebfc", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3ff2e50c-6905-4eeb-ac17-9ee7ab9c5022");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "90c82976-5b02-4114-9a1b-c6f6999aebfc");
+        }
+    }
+}
